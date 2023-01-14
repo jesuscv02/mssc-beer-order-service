fc81c6fe8547d58564f870a67fd3a2915cc9375f
jesuscv02 <jesus.castaneda.havana@gmail.com>
2023-01-14 16:19:51 -0300

    Adding ActiveMq Configuration

---
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -0,0 +1,19 @@
+package guru.sfg.beer.order.service.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTypeIdPropertyName("_type");
+        converter.setTargetType(MessageType.TEXT);
+        return converter;
+    }
+}
